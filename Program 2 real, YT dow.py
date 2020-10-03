fc5e0e0babc2ff6870247678522f7497ef8b9988
FriendedCarrot4z <49000284+FriendedCarrot4z@users.noreply.github.com>
2020-10-03 12:56:47 -0600

    first commit

---
+++ Program 2 real, YT dow.py
@@ -0,0 +1,20 @@
+"""
+Youtube downloader for CS246
+"""
+#gets the import from pytube
+from pytube import YouTube
+
+link = input("Enter the link of YouTube video you want to download:  ")
+yt = YouTube(link)
+
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+#Getting the highest resolution possible
+ys = yt.streams.get_highest_resolution()
+ #Starting download
+print("Downloading...")
+ys.download('/Users/speed/Downloads')
+print("Download completed!!")
---
+++ README.md
@@ -0,0 +1,14 @@
+# Summery
+This program allows you to download youtube videos and store them on your computer
+
+# Development enviroment
+* Python 3.7.8
+* Visual sudio code
+
+# Exicution
+* to exicute the program `Python Program 2 real, YT dow.py`
+ ![Program screen shot](test.png)
+
+# Usfull websites
+* ![Python pytube help guide video](https://www.youtube.com/watch?v=GhPM3gqUyLM/)
+* ![Python pytube help guide](https://towardsdatascience.com/build-a-youtube-downloader-with-python-8ef2e6915d97)
